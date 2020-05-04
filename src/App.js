c255b07051d02b9819a7608c69415221fe0656c5
JungChaeMoon <jungchaemoon@gmail.com>
2020-05-04 19:03:19 +0900

    Add category

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import axios from 'axios';
 import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 const App = () => {
-  return (
-      <NewsList/>
-  )
+    const [Category, setCategory] = useState('all');
+    const onSelect = useCallback(category => setCategory(category), []);
+    return (
+        <div>
+            <div>
+                <Categories category={Category} onSelect={onSelect}/>
+            </div>
+            <div>
+                <NewsList category={Category}/>
+            </div>
+        </div>
+    )
 };
 
 export default App;
